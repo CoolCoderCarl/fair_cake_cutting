1433497434c85ce0e550d303e524aac1d80eb178
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-05-30 20:04:39 +0300

    Update logic & func; update code_quality.yaml

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ persons = 2
 
 
 def generate_cake_tastes() -> List[str]:
-    filling = ["chocolate", "vanilla"]
+    filling = ["chocolate"] #, "vanilla"]
     cherry = randrange(6)
     return [choice(filling), cherry]
 
@@ -17,20 +17,20 @@ def generate_preferences() -> Dict[int, list[str]]:
     return persons_preferences
 
 
-if __name__ == "__main__":
-    cake = generate_cake_tastes()
-    preferences = generate_preferences()
-    print("The cake is " + cake[0] + " with " + str(cake[1]) + " cherries.")
-    print(preferences)
-    w_count = 0
-    n_count = 0
+def who_wants(cake: list, preferences: dict):
+    want_count = 0
+    wont_count = 0
     for keys in preferences:
         for l in range(persons):
             if cake[0] == preferences[keys][l]:
                 print(
                     "Person " + str(list(preferences.keys())[keys]) + " is want a cake"
                 )
-                w_count += 1
+                want_count += 1
+                if want_count == persons:
+                    # print(cake[1])
+                    for v in list(preferences.values()):
+                        print(v[1])
                 break
             elif cake[0] != preferences[keys][l]:
                 print(
@@ -38,10 +38,16 @@ if __name__ == "__main__":
                     + str(list(preferences.keys())[keys])
                     + " is do not want a cake"
                 )
-                n_count += 1
-                if n_count == 2:
+                wont_count += 1
+                if wont_count == persons:
                     print("No one want a cake")
                     exit(1)
                 break
 
 
+if __name__ == "__main__":
+    cake = generate_cake_tastes()
+    preferences = generate_preferences()
+    print("The cake is " + cake[0] + " with " + str(cake[1]) + " cherries.")
+    print(preferences)
+    who_wants(cake, preferences)
